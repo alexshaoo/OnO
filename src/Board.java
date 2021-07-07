58ecbe2d6eb25accc94d058f27371bd2c87b96ee
Ada W <86745853+adabingw@users.noreply.github.com>
2021-07-07 11:09:30 -0600

    Update Board.java

--- src/Board.java
+++ src/Board.java
@@ -4,12 +4,9 @@ public class Board {
 
     public static void main(String[] args) {
         int[][] board = new int[4][4];
-//    int a = 1;
-//	int b = 0;
         Random r = new Random();
         int low = 0;
         int high = 2;
-//	int randomOfTwoInts = new Random().nextBoolean() ? a : b;
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
                 int result = r.nextInt(high-low) + low;
