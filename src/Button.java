72847250b823640c88230fc63b1aa84025294ea4
Ada W <86745853+adabingw@users.noreply.github.com>
2021-07-06 23:03:04 -0600

    Update Button.java

--- src/Button.java
+++ src/Button.java
@@ -16,6 +16,19 @@ public class Button extends JButton {
 
     public void updateValue() {
         this.setText(Integer.toString(this.val + 1));
+        if (val == 1) {
+  				val = 2;
+  				this.setBackground(Color.RED); // for the colour after the click (2)
+  				this.setText(String.valueOf(val));
+  			} else if (val == 0) {
+  				val = 1;
+  				this.setBackground(Color.BLACK); // for the colour after the click (1)
+  				this.setText(String.valueOf(val));
+  			} else if (val == 2) {
+  				val = 0;
+  				this.setBackground(Color.WHITE); // back to 0
+  				this.setText(String.valueOf(val));
+            }
     }
 
 }
