823ab22ae9787affec493b51df00689b7eca49f6
alexs <alexshao90@gmail.com>
2021-07-14 12:55:23 -0700

    updated for more organization -adde new layout maanger -changed game class to be main game window -added user click functionality

--- src/Button.java
+++ src/Button.java
@@ -1,7 +1,6 @@
 import javax.swing.JButton;
 import java.awt.*;
 
-@SuppressWarnings("serial")
 public class Button extends JButton {
 
     int val;
@@ -12,23 +11,22 @@ public class Button extends JButton {
 
         if (val == 1) this.setBackground(Color.BLACK);
         else this.setBackground(Color.WHITE);
+
+        this.addActionListener(new ClickHandler());
     }
 
     public void updateValue() {
-        this.setText(Integer.toString(this.val + 1));
-        if (val == 1) {
-  				val = 2;
-  				this.setBackground(Color.RED); // for the colour after the click (2)
-  				this.setText(String.valueOf(val));
-  			} else if (val == 0) {
-  				val = 1;
-  				this.setBackground(Color.BLACK); // for the colour after the click (1)
-  				this.setText(String.valueOf(val));
-  			} else if (val == 2) {
-  				val = 0;
-  				this.setBackground(Color.WHITE); // back to 0
-  				this.setText(String.valueOf(val));
-            }
+        this.setText(Integer.toString((this.val + 1) % 3));
+        if (val == 0) {
+            val = 1;
+            this.setBackground(Color.RED); // for the colour after the click (1)
+        } else if (val == 1) {
+            val = 2;
+            this.setBackground(Color.BLUE); // for the colour after the click (2)
+        } else {
+            val = 0;
+            this.setBackground(Color.WHITE); // back to 0
+        }
     }
 
 }
