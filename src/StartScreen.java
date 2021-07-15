91745d81287910ab83ec644c36c6684a027aab86
Ada W <86745853+adabingw@users.noreply.github.com>
2021-07-15 12:39:54 -0600

    Update StartScreen.java

--- src/StartScreen.java
+++ src/StartScreen.java
@@ -2,14 +2,16 @@ import javax.swing.*;
 
 public class StartScreen {
 
-    public static int yesNo(String theMessage) {
+/*    public static int yesNo(String theMessage) {
         return JOptionPane.showConfirmDialog(null, theMessage, "WELCOME", JOptionPane.YES_NO_OPTION);
-    }
+    } */
 
     public static void main(String[] args) {
 
-        if (yesNo("Play OnO?") == JOptionPane.YES_OPTION) {
+/*        if (yesNo("Play OnO?") == JOptionPane.YES_OPTION) {
             Game game = new Game();
         } else System.exit(0);
-    }
+    } */
+        
+       	new Opening();
 }
